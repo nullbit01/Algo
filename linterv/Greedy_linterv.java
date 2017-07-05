03294d991e9313b2d7cf30993c31747c9f1337c0
Stefanoiu Alexandru <alexstefanoiu@gmail.com>
2017-07-05 20:06:39 +0300

    Linterv
    Java

---
+++ linterv/Greedy_linterv.java
@@ -0,0 +1,38 @@
+
+package greedy;
+
+class Interval {
+    
+    int a;
+    int b;
+    
+}
+
+public class Greedy_linterv {
+
+    public static void main(String[] args) {
+        
+        Interval[] v = new Interval[6];
+        for(int i = 0; i < 11; i++) {
+            v[i] = new Interval();
+        }
+        
+        v[0].a = -5; v[0].b = 5;
+        v[1].a = 0; v[1].b = 3;
+        v[2].a = 2; v[2].b = 8;
+        v[3].a = 10; v[3].b = 13;
+        v[4].a = 11; v[4].b = 15;
+        v[5].a = 100; v[5].b = 100;
+        
+        int[] v1 = new int[101];
+        int suma = 0;
+        
+        for(int i = 0; i < 6; i++) {
+            
+            
+            
+        }
+        
+    }
+    
+}
